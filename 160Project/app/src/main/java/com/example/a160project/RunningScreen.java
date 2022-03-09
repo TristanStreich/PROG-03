6c9fc2868aeb987e65f676dc2962d3c696a27bb9
Michael Chau <inventormc@gmail.com>
2022-03-09 10:16:15 -0800

    tick numbers for running screen

--- 160Project/app/src/main/java/com/example/a160project/RunningScreen.java
+++ 160Project/app/src/main/java/com/example/a160project/RunningScreen.java
@@ -8,7 +8,11 @@ import androidx.navigation.fragment.NavHostFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ThreadLocalRandom;
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link RunningScreen#newInstance} factory method to
@@ -24,9 +28,11 @@ public class RunningScreen extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private Thread incrementMetricsThread;
 
     public RunningScreen() {
         // Required empty public constructor
+
     }
 
     /**
@@ -54,6 +60,47 @@ public class RunningScreen extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        this.incrementMetricsThread = new Thread(new Runnable() {
+            public void run() {
+                int calories = 0;
+                double miles = 0.0;
+                int steps = 0;
+
+                TextView calories_text = getView().findViewById(R.id.calories_counter);
+                TextView miles_text = getView().findViewById(R.id.miles_counter);
+                TextView steps_text = getView().findViewById(R.id.steps_counter);
+
+                calories_text.setText(Integer.toString(calories));
+                miles_text.setText(String.format("%.2f", miles));
+                steps_text.setText(Integer.toString(steps));
+
+                while (true) {
+                    try {
+                        TimeUnit.SECONDS.sleep(1); // Sleeps the thread for five seconds
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    calories += randInt(1, 10);
+                    miles += randDouble(0, 0.1);
+                    steps += 1;
+
+                    calories_text.setText(Integer.toString(calories));
+                    miles_text.setText(String.format("%.2f", miles));
+                    steps_text.setText(Integer.toString(steps));
+                }
+
+            }
+        });
+
+        Button record = getView().findViewById(R.id.button);
+
+        record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startRun(view);
+            }
+        });
     }
 
     @Override
@@ -61,6 +108,45 @@ public class RunningScreen extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_running_screen, container, false);
+    }
+
+    public static int randInt(int min, int max) {
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
+    }
+
+    public static double randDouble(double min, double max) {
+        return min + Math.random() * (max - min);
+    }
+
+    public void startRun(View view) {
+        Button record = getView().findViewById(R.id.button);
+
+        String stop_text = getResources().getString(R.string.stop);
+        record.setText(stop_text);
+
+        this.incrementMetricsThread.start();
+
+        record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopRun(view);
+            }
+        });
+    }
+
+    public void stopRun(View view) {
+        Button stop = getView().findViewById(R.id.button);
+
+        String record_text = getResources().getString(R.string.record);
+        stop.setText(record_text);
+
+        this.incrementMetricsThread.interrupt();
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startRun(view);
+            }
+        });
     }
 
     //Calling this function will send the user to the home screen
